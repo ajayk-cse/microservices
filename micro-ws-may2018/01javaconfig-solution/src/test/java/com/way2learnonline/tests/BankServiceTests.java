6f45ee3ff000019b07959b641043ee28dcd991a9
ajayk-cse <41698874+ajayk-cse@users.noreply.github.com>
2018-08-11 17:40:00 +0530

    Microservice code by Siva prasad

---
+++ micro-ws-may2018/01javaconfig-solution/src/test/java/com/way2learnonline/tests/BankServiceTests.java
@@ -0,0 +1,38 @@
+package com.way2learnonline.tests;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.way2learnonline.config.ApplicationConfig;
+import com.way2learnonline.config.InfrastructureConfig;
+import com.way2learnonline.service.BankService;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes={ApplicationConfig.class,InfrastructureConfig.class})
+public class BankServiceTests {	
+	
+	@Autowired
+	private BankService bankService;	
+
+	@Test
+	
+	public void testTransfer() throws SQLException{
+		
+		Long transactionId=bankService.transfer(new Long(1), new Long(2), 5000);
+		assertNotNull(transactionId);
+	}
+	
+
+	
+	
+	
+
+}
